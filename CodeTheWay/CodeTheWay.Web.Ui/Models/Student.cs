26db0e00b33068f32b5c7bcf9ce5acb82fbcdab3
Mr. O <osterber@elmbrookschools.org>
2021-06-18 17:28:02 -0500

    Model and View Change - FirstName

--- CodeTheWay/CodeTheWay.Web.Ui/Models/Student.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Models/Student.cs
@@ -9,6 +9,6 @@ namespace CodeTheWay.Web.Ui.Models
     {
         public Guid Id { get; set; }
         public string LastName { get; set; }
-        public string FirstMidName { get; set; }
+        public string FirstName { get; set; }
     }
 }

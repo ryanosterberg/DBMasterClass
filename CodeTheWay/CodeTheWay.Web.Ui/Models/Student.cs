6f5d896fcb8ae89be6d51a19d6a62d90b8cb3bef
Mr. O <osterber@elmbrookschools.org>
2021-06-21 07:52:36 -0500

    Added Age to the Student Model

--- CodeTheWay/CodeTheWay.Web.Ui/Models/Student.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Models/Student.cs
@@ -10,5 +10,6 @@ namespace CodeTheWay.Web.Ui.Models
         public Guid Id { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        public int Age { get; set; }
     }
 }

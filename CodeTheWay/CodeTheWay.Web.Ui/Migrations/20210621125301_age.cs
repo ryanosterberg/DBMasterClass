1162ccc48a06b9d3bc82caf65c57dece6a39b6c2
Mr. O <osterber@elmbrookschools.org>
2021-06-21 07:56:25 -0500

    Migration and alter views

---
+++ CodeTheWay/CodeTheWay.Web.Ui/Migrations/20210621125301_age.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeTheWay.Web.Ui.Migrations
+{
+    public partial class age : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Students");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstMidName",
+                table: "Students",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "FirstMidName",
+                table: "Students");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
